3623bac89a78549a09b5b8e2280aab2ed8aa0424
Viktor Afanasjev <viktor85a@gmail.com>
2017-05-15 12:29:59 +0300

    replace error message

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ if (isset($_SESSION['logdebug']) && $_SESSION['logdebug'] == 1) {
 } else {
     error_reporting(E_ERROR);
 }
+error_reporting(0);
 
 include "headerinc.php";
 include "_logic.php";
@@ -93,4 +94,3 @@ if ($act == "cat" || $act == "none") {
     include "modules/indexmodules/textblock.php";
 }
 include "modules/indexmodules/footer.php";
-error_reporting(0);
